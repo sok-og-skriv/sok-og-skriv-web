402e6ed4e2ecad0e6d57f221af9fd89c49563ddc
Tarje Lavik <tarje.lavik@gmail.com>
2020-02-25 11:59:31 +0100

    Flytt sidebar config til egne filer for enklere redigering for redaksjonen

---
+++ docs/.vuepress/sidebarEn.js
@@ -0,0 +1,82 @@
+module.exports = {
+  '/en/searching/': [
+    '',
+    'plan-your-searches',
+    'searching-techniques',
+    'systematic-searching',
+  ],
+  '/en/reading/': [
+    '',
+    'ways-of-reading',
+    'reading-and-writing',
+    'study-groups',
+    'argumentation-in-text',
+    'academic-genres'
+  ],
+  '/en/writing/': [
+    '',
+    {
+      title: 'Structure and argumentation',
+      children: [
+        'structure-and-argumentation',
+        'structuring-a-thesis',
+        'crafting-an-argument',
+        'the-imrad-format'
+      ]
+    },
+    {
+      title: 'Language and style',
+      children: [
+        'language-and-style',
+        'organising-your-writing',
+        'flow',
+        'non-academic-language'
+      ]
+    },
+    {
+      title: 'The writing process',
+      children: [
+        'the-writing-process',
+        'start-writing',
+        'writing-groups'
+      ]
+    },
+    'formal-requirements',
+    'disseminating-your-thesis',
+  ],
+  '/en/sources-and-referencing/': [
+    '',
+    {
+      title: 'How to cite?',
+      children: [
+        'how-to-cite',
+        'what-to-cite',
+        'abbreviations'
+      ]
+    },
+    {
+      title: 'Why cite sources?',
+      children: [
+        'why-cite-sources',
+        'guidelines-on-academic-ethics',
+        'intellectual-property-rights'
+      ]
+    },
+    'reference-styles',
+    {
+      title: 'Source evaluation',
+      children: [
+        'source-evaluation',
+        'qualitative-evaluation',
+        'assessing-relevance'
+      ]
+    }
+  ],
+  '/en/': [
+    '/en/about/',
+    '/en/video/',
+    '/en/about/contact-information',
+    '/en/about/copyright-for-search-and-write',
+    '/en/about/references'
+  ]
+}
---
+++ docs/.vuepress/sidebarNo.js
@@ -0,0 +1,144 @@
+module.exports = {
+  '/soking/': [
+    '',
+    'planlegg-soket-ditt',
+    'soketeknikker',
+    'systematisk-soking',
+  ],
+  '/lesing/': [
+    '',
+    'lesemater',
+    'lesing-og-skriving',
+    'kollokvie',
+    'argumentasjon-i-tekst',
+    'akademiske-sjangrer'
+  ],
+  '/skriving/': [
+    '',
+    {
+      title: 'Skriveprosessen',
+      children: [
+        'skriveprosessen',
+        'kom-i-gang-a-skrive',
+        'skrivegrupper'
+      ]
+    },
+    {
+      title: 'Struktur og argumentasjon',
+      children: [
+        'struktur-og-argumentasjon',
+        'oppbygning-av-en-oppgave',
+        'redegjor-og-droft',
+        'a-argumentere-i-egen-tekst',
+        'imrod-modellen'
+      ]
+    },
+    {
+      title: 'Akademisk sprak og stil',
+      children: [
+        'akademisk-sprak-og-stil',
+        'skrive-ryddig',
+        'a-skape-flyt',
+        'ikke-akademisk-sprak'
+      ]
+    },
+    'formelle-krav-til-oppsett',
+    'formidling',
+  ],
+  '/kildebruk-og-referanser/': [
+    '',
+    {
+      title: 'Hvordan referere',
+      children: [
+        'hvordan-referere',
+        'hva-refereres',
+        'forkortelser'
+      ]
+    },
+    {
+      title: 'Hvorfor referere',
+      children: [
+        'hvorfor-referere',
+        'etiske-retningslinjer-i-akademia',
+        'opphavsrettslige-forhold'
+      ]
+    },
+    /* {
+      title: 'Referansestiler',
+      children: [
+        'referansestiler/',
+        'referansestiler/apa-6th',
+        'referansestiler/chicago-fotnoter',
+        'referansestiler/chicago-forfatter-aar',
+        'referansestiler/mla',
+        'referansestiler/harvard',
+        'referansestiler/vancouver',
+      ]
+    }, */
+    
+    {
+      title: 'Kildevurdering',
+      children: [
+        'kildevurdering',
+        'kvalitative-vurderinger',
+        'vurdering-av-relevans'
+      ]
+    }
+  ],
+  '/referansestiler/apa/': [
+    '',
+    'boeker',
+    'artikler',
+    'offentlige-kilder',
+    'annet'
+  ],
+  '/referansestiler/chicago-forfatter-aar/': [
+    '',
+    /* 'boeker',
+    'artikler',
+    'offentlige-kilder',
+    'annet' */
+  ],
+  '/referansestiler/chicago-fotnoter/': [
+    '',
+    /* 'boeker',
+    'artikler',
+    'offentlige-kilder',
+    'annet' */
+  ],
+  '/referansestiler/harvard/': [
+    '',
+    /* 'boeker',
+    'artikler',
+    'offentlige-kilder',
+    'annet' */
+  ],
+  '/referansestiler/mla/': [
+    '',
+    /* 'boeker',
+    'artikler',
+    'offentlige-kilder',
+    'annet' */
+  ],
+  '/referansestiler/vancouver/': [
+    '',
+    /* 'boeker',
+    'artikler',
+    'offentlige-kilder',
+    'annet' */
+  ],
+  '/om/brukertest/': [
+    '',
+    'eksempel2',
+    'eksempel3',
+    'eksempel4',
+    'eksempel5'
+  ],
+  '/': [
+    '/om/',
+    '/video/',
+    '/om/kontaktinformasjon',
+    '/om/rettigheter-til-sok-og-skriv',
+    '/om/referanser'
+  ]
+}
