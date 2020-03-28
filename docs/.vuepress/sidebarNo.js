9859ddf33de3e83925fb49a293d22a3c18ae0bc2
Tarje Sælen Lavik <tarje.lavik@uib.no>
2020-03-28 17:53:55 +0000

    Update docs/.vuepress/sidebarNo.js

--- docs/.vuepress/sidebarNo.js
+++ docs/.vuepress/sidebarNo.js
@@ -18,6 +18,7 @@ module.exports = {
     '',
     'kom-i-gang-a-skrive',
     'struktur',
+    'argumentere-redegjore-drofte',
     'oppbygning-av-en-oppgave',
     'imrad-modellen',
     'akademisk-sprak-og-stil',
