18f033aa629c6af3b37fec9a7c225f42579cf260
Tarje Lavik <tarje.lavik@gmail.com>
2021-04-08 13:06:24 +0200

    Legg til iframe embed kode i bunnen av side

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -5,20 +5,11 @@
     @touchstart="onTouchStart"
     @touchend="onTouchEnd"
   >
-    <Navbar
-      v-if="shouldShowNavbar"
-      @toggle-sidebar="toggleSidebar"
-    />
+    <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar" />
 
-    <div
-      class="sidebar-mask"
-      @click="toggleSidebar(false)"
-    />
+    <div class="sidebar-mask" @click="toggleSidebar(false)" />
 
-    <Sidebar
-      :items="sidebarItems"
-      @toggle-sidebar="toggleSidebar"
-    >
+    <Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar">
       <template #top>
         <slot name="sidebar-top" />
       </template>
@@ -29,22 +20,27 @@
 
     <Home v-if="$page.frontmatter.home" />
 
-    <Page
-      v-else
-      :sidebar-items="sidebarItems"
-    >
+    <Page v-else :sidebar-items="sidebarItems">
       <template #top>
         <slot name="page-top" />
       </template>
+
       <template #bottom>
         <slot name="page-bottom" />
+
         <footer class="theme-default-content">
           <div class="partners">
             <div class="partner">
               <a href="https://www.hvl.no">
                 <picture>
-                  <source media="(max-width: 809px)" srcset="/partners/hvl-icon.jpg">
-                  <img src="/partners/hvl-logo.jpg" alt="Høyskolen på Vestlandet"/>
+                  <source
+                    media="(max-width: 809px)"
+                    srcset="/partners/hvl-icon.jpg"
+                  />
+                  <img
+                    src="/partners/hvl-logo.jpg"
+                    alt="Høyskolen på Vestlandet"
+                  />
                 </picture>
               </a>
               <div class="title">
@@ -54,8 +50,14 @@
             <div class="partner">
               <a href="https://www.uib.no">
                 <picture>
-                  <source media="(max-width: 809px)" srcset="/partners/uib-icon.png">
-                  <img src="/partners/uib-logo.png" alt="Universitetet i Bergen"/>
+                  <source
+                    media="(max-width: 809px)"
+                    srcset="/partners/uib-icon.png"
+                  />
+                  <img
+                    src="/partners/uib-logo.png"
+                    alt="Universitetet i Bergen"
+                  />
                 </picture>
               </a>
               <div class="title">
@@ -65,8 +67,14 @@
             <div class="partner">
               <a href="https://www.uio.no">
                 <picture>
-                  <source media="(max-width: 809px)" srcset="/partners/uio-icon.png">
-                  <img src="/partners/uio-logo.png" alt="Universitetet i Oslo"/>
+                  <source
+                    media="(max-width: 809px)"
+                    srcset="/partners/uio-icon.png"
+                  />
+                  <img
+                    src="/partners/uio-logo.png"
+                    alt="Universitetet i Oslo"
+                  />
                 </picture>
               </a>
               <div class="title">
@@ -76,8 +84,11 @@
             <div class="partner">
               <a href="https://www.nb.no">
                 <picture>
-                  <source media="(max-width: 809px)" srcset="/partners/nb-icon.png">
-                  <img src="/partners/nb-logo.png" alt="Nasjonalbiblioteket"/>
+                  <source
+                    media="(max-width: 809px)"
+                    srcset="/partners/nb-icon.png"
+                  />
+                  <img src="/partners/nb-logo.png" alt="Nasjonalbiblioteket" />
                 </picture>
               </a>
               <div class="title">
@@ -88,12 +99,20 @@
           <div class="container two-column footer-links">
             <div class="align-right">
               <div><a href="/om/">Om Søk & skriv</a></div>
-              <div><a href="/om/kontaktinformasjon.html">Kontaktinformasjon</a></div>
-              <div><a href="/om/sok-og-skriv-i-undervisning.html">Søk & skriv i undervisning</a></div>
+              <div>
+                <a href="/om/kontaktinformasjon.html">Kontaktinformasjon</a>
+              </div>
+              <div>
+                <a href="/om/sok-og-skriv-i-undervisning.html"
+                  >Søk & skriv i undervisning</a
+                >
+              </div>
             </div>
             <div class="align-left">
               <div><a href="/en/about/">About Search & write</a></div>
-              <div><a href="/en/about/contact-information.html">Contact</a></div>
+              <div>
+                <a href="/en/about/contact-information.html">Contact</a>
+              </div>
             </div>
           </div>
         </footer>
@@ -103,7 +122,7 @@
 </template>
 
 <style lang="stylus" scoped>
-footer .partners  
+footer .partners
   grid-template-columns: 1fr 1fr
   grid-gap: 2.5rem
   display: grid
@@ -129,108 +148,106 @@ footer .partners
 </style>
 
 <script>
-import Home from '@parent-theme/components/Home.vue'
-import Navbar from '@parent-theme/components/Navbar.vue'
-import Page from '@parent-theme/components/Page.vue'
-import Sidebar from '@parent-theme/components/Sidebar.vue'
-import { resolveSidebarItems } from '@parent-theme//util'
+import Home from "@parent-theme/components/Home.vue";
+import Navbar from "@parent-theme/components/Navbar.vue";
+import Page from "../components/Page.vue";
+import Sidebar from "@parent-theme/components/Sidebar.vue";
+import { resolveSidebarItems } from "@parent-theme/util";
 
 export default {
-  name: 'Layout',
+  name: "Layout",
 
   components: {
     Home,
     Page,
     Sidebar,
-    Navbar
+    Navbar,
   },
 
-  data () {
+  data() {
     return {
-      isSidebarOpen: false
-    }
+      isSidebarOpen: false,
+    };
   },
 
   computed: {
-    shouldShowNavbar () {
-      const { themeConfig } = this.$site
-      const { frontmatter } = this.$page
-      if (
-        frontmatter.navbar === false
-        || themeConfig.navbar === false) {
-        return false
+    shouldShowNavbar() {
+      const { themeConfig } = this.$site;
+      const { frontmatter } = this.$page;
+      if (frontmatter.navbar === false || themeConfig.navbar === false) {
+        return false;
       }
       return (
-        this.$title
-        || themeConfig.logo
-        || themeConfig.repo
-        || themeConfig.nav
-        || this.$themeLocaleConfig.nav
-      )
+        this.$title ||
+        themeConfig.logo ||
+        themeConfig.repo ||
+        themeConfig.nav ||
+        this.$themeLocaleConfig.nav
+      );
     },
 
-    shouldShowSidebar () {
-      const { frontmatter } = this.$page
+    shouldShowSidebar() {
+      const { frontmatter } = this.$page;
       return (
-        !frontmatter.home
-        && frontmatter.sidebar !== false
-        && this.sidebarItems.length
-      )
+        !frontmatter.home &&
+        frontmatter.sidebar !== false &&
+        this.sidebarItems.length
+      );
     },
 
-    sidebarItems () {
+    sidebarItems() {
       return resolveSidebarItems(
         this.$page,
         this.$page.regularPath,
         this.$site,
         this.$localePath
-      )
+      );
     },
 
-    pageClasses () {
-      const userPageClass = this.$page.frontmatter.pageClass
+    pageClasses() {
+      const userPageClass = this.$page.frontmatter.pageClass;
       return [
         {
-          'no-navbar': !this.shouldShowNavbar,
-          'sidebar-open': this.isSidebarOpen,
-          'no-sidebar': !this.shouldShowSidebar
+          "no-navbar": !this.shouldShowNavbar,
+          "sidebar-open": this.isSidebarOpen,
+          "no-sidebar": !this.shouldShowSidebar,
         },
-        userPageClass
-      ]
-    }
+        userPageClass,
+      ];
+    },
   },
 
-  mounted () {
+  mounted() {
     this.$router.afterEach(() => {
-      this.isSidebarOpen = false
-    })
+      this.isSidebarOpen = false;
+    });
   },
 
   methods: {
-    toggleSidebar (to) {
-      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-      this.$emit('toggle-sidebar', this.isSidebarOpen)
+    toggleSidebar(to) {
+      this.isSidebarOpen = typeof to === "boolean" ? to : !this.isSidebarOpen;
+      this.$emit("toggle-sidebar", this.isSidebarOpen);
     },
 
     // side swipe
-    onTouchStart (e) {
+    onTouchStart(e) {
       this.touchStart = {
         x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
-      }
+        y: e.changedTouches[0].clientY,
+      };
     },
 
-    onTouchEnd (e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
+    onTouchEnd(e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x;
+      const dy = e.changedTouches[0].clientY - this.touchStart.y;
       if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
         if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
+          this.toggleSidebar(true);
         } else {
-          this.toggleSidebar(false)
+          this.toggleSidebar(false);
         }
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
